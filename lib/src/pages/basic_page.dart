9f4414b2c12a04e12b7c8f479cea228281425abd
Jordi <jordi.bravo.bad@gmail.com>
2021-03-05 22:47:21 -0300

    Refactoring with functions

--- lib/src/pages/basic_page.dart
+++ lib/src/pages/basic_page.dart
@@ -9,37 +9,45 @@ class BasicPage extends StatelessWidget {
       child: Scaffold(
           body: Column(
         children: [
-          Image(
-            image: NetworkImage(
-                'https://llandscapes-10674.kxcdn.com/wp-content/uploads/2019/07/lighting.jpg'),
-          ),
-          Row(
-            children: [
-              Container(
-                padding: EdgeInsets.all(30),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(right: 60, bottom: 10),
-                      child: Text(
-                        'Beautiful Mountains Travel',
-                        style: Theme.of(context).textTheme.button,
-                      ),
-                    ),
-                    Text(
-                      'Puente Alto, Chile',
-                      style: Theme.of(context).textTheme.caption,
-                    )
-                  ],
-                ),
-              ),
-              Icon(Icons.star, color: Colors.redAccent),
-              Text('41'),
-            ],
-          ),
+          _createImage(),
+          _createHeader(context),
         ],
       )),
+    );
+  }
+
+  Widget _createImage() {
+    return Image(
+      image: NetworkImage(
+          'https://llandscapes-10674.kxcdn.com/wp-content/uploads/2019/07/lighting.jpg'),
+    );
+  }
+
+  Widget _createHeader(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.only(right: 60, bottom: 10),
+                child: Text(
+                  'Beautiful Mountains Travel',
+                  style: Theme.of(context).textTheme.button,
+                ),
+              ),
+              Text(
+                'Puente Alto, Chile',
+                style: Theme.of(context).textTheme.caption,
+              )
+            ],
+          ),
+        ),
+        Icon(Icons.star, color: Colors.redAccent),
+        Text('41'),
+      ],
     );
   }
 }
