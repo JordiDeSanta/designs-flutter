8d0847943f900b71a58522e0c17f2a9e569f41d2
Jordi <jordi.bravo.bad@gmail.com>
2021-03-05 23:49:54 -0300

    Finishing First Page

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -9,17 +9,18 @@ class ScrollPage extends StatelessWidget {
         body: PageView(
       scrollDirection: Axis.vertical,
       children: [
-        _createPage1(),
+        _createPage1(context),
         _createPage2(),
       ],
     ));
   }
 
-  Widget _createPage1() {
+  Widget _createPage1(BuildContext context) {
     return Stack(
       children: [
         _backgroundColor(),
         _createImage(),
+        _createTexts(context),
       ],
     );
   }
@@ -34,8 +35,13 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _createImage() {
-    return Image(
-      image: AssetImage('assets/img/scroll-1.png'),
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/img/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
     );
   }
 
@@ -44,6 +50,40 @@ class ScrollPage extends StatelessWidget {
       width: double.infinity,
       height: double.infinity,
       color: Color.fromRGBO(122, 236, 203, 1),
+    );
+  }
+
+  Widget _createTexts(BuildContext context) {
+    final style = TextStyle(
+      color: Colors.white,
+      fontSize: 50,
+    );
+
+    return Center(
+      child: Container(
+        padding: EdgeInsets.only(top: 80, bottom: 10),
+        child: Column(
+          children: [
+            Text(
+              '11°',
+              style: style,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Monday',
+              style: style,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 410),
+            Icon(
+              Icons.keyboard_arrow_down,
+              size: 70.0,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
